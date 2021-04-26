591d556004534a3681f179bf4c3bb637ec8e98f0
Jacob Roe <jroenavy24@gmail.com>
2021-04-26 12:06:53 -0700

    first commit

---
+++ loop_chall.js
@@ -0,0 +1,31 @@
+//Print Odds 1-20
+var oddNumber = 1;
+for (var i = 0; i < 10; i++) {
+    console.log(oddNumber);
+    oddNumber += 2;
+}
+
+//Print The Sequence
+var randomNumber = 4
+for (var i = 0; i < 6; i++) {
+    console.log(randomNumber);
+    randomNumber -= 1.5 
+}
+
+//Sigma
+const number = int;
+let sum = 0, i = 1;
+while(i <= number) {
+    sum += i;
+    i++
+}
+console.log(sum);
+
+//Factorial
+var number = 1e8;
+let sum = 1, n = 1;
+while(sum <= number) {
+    sum *= n;
+    n++
+}
+console.log(sum)
